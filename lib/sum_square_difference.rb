f8e47aca6ce4bbba44da5ebbe8315a2f20c44cf7
James Clement <jameseclement@gmail.com>
2019-04-15 09:28:50 -0400

    done

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,12 @@
 # Implement your procedural solution here!
+def sum_of_squares(max)
+  (1..max).map{|num| num**2}.inject{|x,y| x+y}
+end
+
+def square_of_sums(max)
+  (1..max).inject{|x,y| x+y }**2
+end
+
+def sum_square_difference(num)
+  square_of_sums(num) - sum_of_squares(num)
+end
